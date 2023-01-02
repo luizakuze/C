c59a417024733056aa02891f4ca6d2d70fcf8733
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-01-02 15:40:26 -0300

    Create Char.c

---
+++ Condicionais/Char.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Definindo Variaveis
+    char letra = 'x';
+
+    // Condicional Simples
+    if (letra == 'x')
+        printf("\n A letra eh X.");
+
+    // Codigo da letra
+    printf("\nCodigo da letra = %d", letra);
+
+    if (letra == 120)
+        printf("\n A letra eh X.");
+
+    return 0;
+}
