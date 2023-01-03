3a38948b98904c7c0b6d36049612a5635e5d56e5
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-01-03 00:48:07 -0300

    Create Define.c

---
+++ Variáveis/Define.c
@@ -0,0 +1,11 @@
+// Quanto utilizar muitas vezes um valor no decorrer do programa, definir uma variável com o valor fixo 
+
+#include <stdio.h>
+#define TAM 10
+
+int main ()
+{
+  printf("%d", TAM);
+  
+  return 0; 
+}
