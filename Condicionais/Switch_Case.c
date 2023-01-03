3b2ef35f13ad8c534abbad25a8461dc358c0feaa
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-01-02 23:52:05 -0300

    Create Switch_Case.c

---
+++ Condicionais/Switch_Case.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Definindo Variáveis
+    int a = 2;
+    char b = 'y';
+ 
+    //Código de exemplo com if
+    if(a == 1){
+        printf("\n Opcao escolhida: 1");
+    }else if(a == 2){
+        printf("\n Opcao escolhida: 2");
+    }else if(a == 3){
+        printf("\n Opcao escolhida: 3");
+    }else{
+        printf("\n Opcao invalida");
+    }
+ 
+    //O mesmo código de cima, adaptado ao Switch
+    switch(a){
+        case 1:
+            printf("\n Opcao escolhida: 1");
+            break;
+        case 2:
+            printf("\n Opcao escolhida: 2");
+            break;
+        case 3:
+            printf("\n Opcao escolhida: 2");
+            break;
+        default: //Para qualquer outro valor que A assumir
+            printf("\n Opcao invalida");
+            break;
+    }
+ 
+    //Switch com Char
+    switch(b){
+        case 'x':
+            printf("\n A letra eh x");
+            break;
+        default:
+            printf("\n Opcao invalida");
+            break;
+    }
+ 
+    //Pausa o programa após executar
+    system("pause");
+ 
+}
