24e136c4cdbe37257f092bc19ed6346088518c0c
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-01-07 22:19:45 -0300

    Create Função_Void.c

---
+++ Ponteiros_Funções_Tipos/Função_Void.c
@@ -0,0 +1,23 @@
+// Função VOID = Função sem retorno
+
+#include <stdio.h>
+
+// Função que desenha borda
+void desenhaBorda () {
+
+    printf("|-----------------|\n");
+}
+
+int main ()
+{
+    // Chamando a função
+    desenhaBorda();
+    
+    // Imprimindo an tela
+    printf("hi\n");
+    
+    // Chamando a função
+    desenhaBorda();
+
+    return 0;
+}
