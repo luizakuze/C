bdeb8e723aad0d3017644821e4efe138d53f7933
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-01-17 15:25:38 -0300

    Update Empilhar(Push).c

--- Pilhas/Empilhar(Push).c
+++ Pilhas/Empilhar(Push).c
@@ -9,6 +9,7 @@ using namespace std;
 
 void imprime_vetor (int vetor[TAM])
 {
+    cout << endl;
     for (int i = 0; i < TAM; i++)
         cout << vetor[i] << "  ";
 
@@ -16,7 +17,6 @@ void imprime_vetor (int vetor[TAM])
 
 void pilha_push (int pilha[TAM], int valor, int *topo)
 {
-    cout << endl;
     // Caso não possa colocar mais valores
     if (*topo == TAM - 1) {
         cout << "Pilha cheia";
@@ -37,7 +37,6 @@ int main ()
     // Empilhando
     pilha_push(pilha, 5, &topo);
     pilha_push(pilha, 8, &topo);
-
 
     imprime_vetor(pilha);
 
